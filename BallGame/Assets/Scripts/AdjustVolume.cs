164f3a8d5f0f65c4c8a09e859ba86ca5757159b7
Michael Matta <michael_matta@student.wayland.k12.ma.us>
2017-12-18 17:24:43 -0500

    Created basic data saving template

--- BallGame/Assets/Scripts/AdjustVolume.cs
+++ BallGame/Assets/Scripts/AdjustVolume.cs
@@ -5,8 +5,15 @@ using UnityEngine.UI;
 public class AdjustVolume : MonoBehaviour {
 
 	public Slider slider;
+
+	void Start() {
+		DataController.control.Load ();
+	}
+
 	public void AdjustVol () {
 		this.GetComponent<AudioSource> ().volume = slider.value;
+		DataController.control.experience += 2;
+		DataController.control.Save ();
 	}
 
 }
---
+++ BallGame/Assets/Scripts/DataController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class DataController : MonoBehaviour {
+
+	public static DataController control; //can be called from anywhere with "DataController.control"
+
+	public float health;
+	public float experience;
+
+	void Awake () {
+		if (control == null) {
+			DontDestroyOnLoad (gameObject);
+			control = this;
+		} else if (control != this) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnGUI () {
+		GUI.Label (new Rect (10, 10, 100, 30), "XP:" + experience);
+	}
+
+	public void Save() {
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream file = File.Create (Application.persistentDataPath + "/playerInfo.dat"); //data extension can be anything
+
+		PlayerData data = new PlayerData();
+		//data.health = health;
+		data.experience = experience;
+
+		bf.Serialize (file, data); //you can save this data to all sorts of stuff (e.g. internet)
+		file.Close ();
+	}
+
+	public void Load() {
+		if (File.Exists (Application.persistentDataPath + "/playerInfo.dat")) {
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream file = File.Open (Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+			PlayerData data = (PlayerData) bf.Deserialize (file);
+			file.Close ();
+
+			//health = data.health;
+			experience = data.experience;
+		}
+	}
+}
+
+[Serializable]
+class PlayerData { //we can add constructers and stuff but this is just the outline
+
+	//public float health;
+	public float experience;
+
+}
